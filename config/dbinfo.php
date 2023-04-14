1f55dafe4533fcd377f05e828789404887980c18
Sai Deepak <78069444+kapeedias@users.noreply.github.com>
2023-04-14 16:02:41 -0700

    Architecture

---
+++ config/dbinfo.php
@@ -0,0 +1,9 @@
+<?php
+
+// Database credentials
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'your_database_name');
+define('DB_USER', 'your_database_user');
+define('DB_PASSWORD', 'your_database_password');
+
+?>
